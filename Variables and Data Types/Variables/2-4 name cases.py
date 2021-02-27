59f7ec94e9d1ae9abe1198ea45f0f0a949d238c8
Kheder S Assaad <79359100+DevKheder@users.noreply.github.com>
2021-02-27 21:19:01 +0200

    Create 2-4 name cases.py

---
+++ Variables and Data Types/Variables/2-4 name cases.py
@@ -0,0 +1,13 @@
+#2-4 Name Cases
+
+#storing a person's name in a variable
+name = "tinDra"
+
+#Printing name in lowercase
+print(name.lower())
+
+#printing name in uppercase
+print(name.upper())
+
+#printing name in title/Capitalize
+print(name.title())
