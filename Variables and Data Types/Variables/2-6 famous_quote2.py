7e68cfa00fe2094de9c7115fdaa3ba04a6e2cf8d
Kheder S Assaad <79359100+DevKheder@users.noreply.github.com>
2021-02-27 21:35:21 +0200

    Create 2-6 famous_quote2.py

---
+++ Variables and Data Types/Variables/2-6 famous_quote2.py
@@ -0,0 +1,6 @@
+#2-5 Famous Quote
+author = "abraham lincoln"
+
+quote = "In the end, it's not the years in your life that count. It's the life in your years."
+
+print(author.title() + " once said, " + '"' + quote + '"')
