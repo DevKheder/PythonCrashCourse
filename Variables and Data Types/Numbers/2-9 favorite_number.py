77c0026be121f0398d18aae41456eebf7db08476
Kheder S Assaad <79359100+DevKheder@users.noreply.github.com>
2021-02-27 22:06:24 +0200

    Create 2-9 favorite_number.py

---
+++ Variables and Data Types/Numbers/2-9 favorite_number.py
@@ -0,0 +1,8 @@
+#2-9 Favorite Number
+
+favorite_number = 25
+
+print("Hi Anna, I heard that you're throwing a party next Monday.\n"+
+"Glad I received your invitation last week, I might not be able to come\n"+
+"because i'm too busy with working and sadly I couldn't call it off, anyway dear\n"+
+"\tHappy " + str(favorite_number) + " birthday, stay safe and happy!!!")
