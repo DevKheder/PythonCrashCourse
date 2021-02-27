30ac167990697e3bd7a7d750fcfcd6d4914118af
Kheder S Assaad <79359100+DevKheder@users.noreply.github.com>
2021-02-27 21:32:38 +0200

    Delete 2-5 famous_quote.py

--- Variables and Data Types/Variables/2-5 famous_quote.py
+++
@@ -1,7 +0,0 @@
-#2-5 Famous Quote
-
-author = "abraham lincoln"
-
-quote = "In the end, it's not the years in your life that count. It's the life in your years."
-
-print(author.title() + " once said, " + '"' + quote + '"')
