504abc3b01e3c26d86e6900fc72a8ab916e2c7e9
Kheder S Assaad <79359100+DevKheder@users.noreply.github.com>
2021-02-27 21:16:01 +0200

    Create 2-3 Personal Message.py

---
+++ Variables and Data Types/Variables/2-3 Personal Message.py
@@ -0,0 +1,10 @@
+#2-3 Personal Message
+
+#creating a variable
+name = "kheder"
+#creating a message and concatinating it with the name variable
+message = "Hello " + name.title() +", welcome to the world of python."
+print(message)
+#or
+print("Hello " + name.title() + ", welcome to the world of python.")
+
