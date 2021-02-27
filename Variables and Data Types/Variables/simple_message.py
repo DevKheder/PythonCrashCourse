673cc1aae1a169f56cab9699055fadd69d062858
Kheder S Assaad <79359100+DevKheder@users.noreply.github.com>
2021-02-27 11:30:00 +0200

    Update simple_message.py
    Content added

--- Variables and Data Types/Variables/simple_message.py
+++ Variables and Data Types/Variables/simple_message.py
@@ -1,1 +1,12 @@
+'''
+2-1 Simple Message
 
+'''
+
+#creating a variable
+
+message = "Hello Pythonists, World. Considerably Python stands as your great choice to kick-off in programming and sift through the complex field of IT"
+
+#now printing the messagge
+
+print(message)
