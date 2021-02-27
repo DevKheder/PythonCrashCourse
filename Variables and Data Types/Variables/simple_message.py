24aabc0f2c0f0222c7342747c2c476aec2a97de6
Kheder S Assaad <79359100+DevKheder@users.noreply.github.com>
2021-02-27 11:49:18 +0200

    Update
    Added Message 2-2 excercise

--- Variables and Data Types/Variables/simple_message.py
+++ Variables and Data Types/Variables/simple_message.py
@@ -10,3 +10,26 @@ message = "Hello Pythonists, World. Considerably Python stands as your great cho
 #now printing the messagge
 
 print(message)
+
+'''
+
+2-2 Simple Message
+
+'''
+
+#storing a message in a variable
+
+message_1 = "In mathematics, you don't understand things. You just get used to them."
+
+#printing the message
+print(message_1)
+
+#now changing the value of the variable
+
+message_1  = "Five out of four people have trouble with fractions."
+
+#now printing the message
+
+print(message_1)
+
+#values in the variable (message_1) are both quotes that belong to: John von Neumann/Steven Wright- respectively.
