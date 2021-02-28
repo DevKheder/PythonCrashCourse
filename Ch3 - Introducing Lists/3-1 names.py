caec1c7509edf8c160654fc677afd0b4eb9380fb
Kheder S Assaad <79359100+DevKheder@users.noreply.github.com>
2021-02-28 16:56:19 +0200

    Create 3-1 names.py

---
+++ Ch3 - Introducing Lists/3-1 names.py
@@ -0,0 +1,13 @@
+#3 - 1 names
+
+friends_names = ['liam', 'kheder', 'tawfeek', 'tindra']
+
+print(friends_names[0].title())
+print(friends_names[1].title())
+print(friends_names[2].title())
+print(friends_names[3].title())
+
+#or we'd make it shorter by using a for loop
+
+for name in friends_names:
+	print(name.title())
