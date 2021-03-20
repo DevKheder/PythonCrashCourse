738469f6a1da0b161cabf617e6f1ed7b65f6eafc
Kheder S Assaad <79359100+DevKheder@users.noreply.github.com>
2021-03-20 20:50:33 +0200

    Create 3 - 9 dinner_guests.py

---
+++ Ch3 - Introducing Lists/3 - 9 dinner_guests.py
@@ -0,0 +1,18 @@
+#3 - 9 Dinner Guests
+
+'''
+i'm not going to use the pre-excersice but instead 
+i'll create another list
+
+'''
+
+modes_of_transport = ['railways', 'roadways', 
+						'airways', 'waterways', 
+						'pipelines',
+						]
+						
+print("Mainly there are " + 
+		str(len(modes_of_transport)) + 
+		" types of transportation"
+		)
+
