d901dec34c4cca94311997b93822aadddaa5801f
Kheder S Assaad <79359100+DevKheder@users.noreply.github.com>
2021-09-10 21:56:11 +0300

    Create 2-3 personal_message.py

---
+++ Chapter 2 - Variables an/2-3 personal_message.py
@@ -0,0 +1,8 @@
+#storing name in variable
+name = "kheder"
+
+""" printing a message for the name and adding a title method 
+to capitalize the first letter of the name 'k'"""
+
+
+print("Hello, " + name.title() + ". would you like to participate in learnin' python?")
