d269021bfbea3cbae9953e0916f96e7e2324d770
Kheder S Assaad <79359100+DevKheder@users.noreply.github.com>
2021-09-13 23:54:26 +0300

    Create 2-6 famous_quote2.py

---
+++ Chapter 2 - Variables an/2-6 famous_quote2.py
@@ -0,0 +1,4 @@
+my_favorite_quote = "A person who never made a mistake never tried anything new"
+quote_author = "albert enstein"
+
+print(quote_author.title() + ' once said, ' + '"' + my_favorite_quote + '"')
