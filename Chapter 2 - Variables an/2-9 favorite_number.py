c71decfe5244bc8cb4619bc7b7a0eaf7c16039a2
Kheder S Assaad <79359100+DevKheder@users.noreply.github.com>
2021-09-14 00:04:09 +0300

    Create 2-9 favorite_number.py

---
+++ Chapter 2 - Variables an/2-9 favorite_number.py
@@ -0,0 +1,13 @@
+my_favorite_num = 5
+
+print("my favorite number is " + str(my_favorite_num))
+
+# this is a comment
+""" this is a multiply
+  com m e nt ary
+  ..... """
+
+
+# importing the zen of python
+
+import this
