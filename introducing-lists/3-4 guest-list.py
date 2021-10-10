3a906d6409191043eed2c2af1024e8a63d034685
Kheder S Assaad <79359100+DevKheder@users.noreply.github.com>
2021-10-10 19:34:47 +0300

    Create 3-4 guest-list.py

---
+++ introducing-lists/3-4 guest-list.py
@@ -0,0 +1,11 @@
+# creating a list of guests
+
+guests = ["modar", "sara", "samer", "ahmed", "mouhammed"]
+
+# printing a message for each item
+
+print("Howdyy, " + guests[0].title() + " I would like to invite you for a dinner")
+print("Howdyy, " + guests[1].title() + " I would like to invite you for a dinner")
+print("Howdyy, " + guests[2].title() + " I would like to invite you for a dinner")
+print("Howdyy, " + guests[3].title() + " I would like to invite you for a dinner")
+print("Howdyy, " + guests[4].title() + " I would like to invite you for a dinner")
