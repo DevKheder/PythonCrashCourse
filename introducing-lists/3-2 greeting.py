f28b52ead1b7013602854b948441513422c702a6
Kheder S Assaad <79359100+DevKheder@users.noreply.github.com>
2021-10-10 19:21:14 +0300

    Create 3-2 greeting.py

---
+++ introducing-lists/3-2 greeting.py
@@ -0,0 +1,17 @@
+
+# making a list of friends' names
+friends = ["kheder", "modar", "sandy", "amani"]
+# printing a message for each name!:
+# 1- using a loop
+
+for name in friends:
+	print("Hello " + name.title() + 
+	" you're very welcome to join the course")
+	
+	
+# 2- doing it for each item.
+
+print("Hello, " + friends[0].title() + " we're vary happy to see here!!")
+print("Hello, " + friends[1].title() + " we're vary happy to see here!!")
+print("Hello, " + friends[2].title() + " we're vary happy to see here!!")
+print("Hello, " + friends[3].title() + " we're vary happy to see here!!")
