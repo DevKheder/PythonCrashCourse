9727af6cd886c634bb6ce975ffca9cb9722e772c
Kheder S Assaad <79359100+DevKheder@users.noreply.github.com>
2021-10-10 19:28:04 +0300

    Create 3-3 my-own-list.py

---
+++ introducing-lists/3-3 my-own-list.py
@@ -0,0 +1,14 @@
+# my list of favorite transportation mode
+
+transport_modes = ["car", "train", "bus", "airplane"]
+
+# printing out each item with a message
+print("I would like to own a " + transport_modes[0].title())
+print("I would like to own a " + transport_modes[1].title())
+print("I would like to own a " + transport_modes[2].title())
+print("I would like to own a " + transport_modes[3].title())
+
+# using the loop method
+
+for mode in tranport_modes:
+	print("I would like to own a " + transport_modes.title())
