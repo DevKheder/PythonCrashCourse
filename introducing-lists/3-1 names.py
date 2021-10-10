edcd0b3701bcfd829c20a31ffcbc9714bc06bc31
Kheder S Assaad <79359100+DevKheder@users.noreply.github.com>
2021-10-10 19:16:23 +0300

    Create 3-1 names.py

---
+++ introducing-lists/3-1 names.py
@@ -0,0 +1,7 @@
+# making a list of friends' names
+friends = ["kheder", "modar", "sandy", "amani"]
+# now printing each name indvidually
+print(friends[0].upper())
+print(friends[1])
+print(friends[2])
+print(friends[3])
